45f72825b35173605102027b3774ec3f0116850b
vectorx <uxiahnan@outlook.com>
2024-07-08 15:43:41 +0800

    build: 移动端后端开发-项目初始化配置
    1、Knife4j: OpenAPI、GroupedOpenApi
    2、@ConditionalOnProperty：配置项存在时，才会加载，避免报错
    3、default-flat-param-object：json做打平处理，方便使用Knife4j调试

--- lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -2,6 +2,7 @@ package com.atguigu.lease.common.minio;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(MinioProperties.class)
+// 配置项存在时，才会加载，避免报错
+@ConditionalOnProperty(name = "minio.endpoint")
 public class MinioConfiguration
 {
     @Autowired
---
+++ lease/web/web-app/src/main/java/com/atguigu/lease/AppWebApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author VectorX
+ * @version V1.0
+ * @description
+ * @date 2024-07-08 15:31:11
+ */
+@SpringBootApplication
+public class AppWebApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebApplication.class);
+    }
+}
